120821aaa28face74e59d68af883f2a8d1c1f668
Pouria <108181964+pouriavj@users.noreply.github.com>
2025-10-25 23:00:13 +0330

    Initial Commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Beyonce",
+    imgURL:
+      "https://cdn.britannica.com/59/204159-050-5055F2A9/Beyonce-2013.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    id: 2,
+    name: "Jack Bauer",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/en/b/b9/Jack_Bauer.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Brad Pitt",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSEuv_rs1B64_PrrnLOYnPBD_nA0tbhLnfy6MuYk6dtTG-RqTDM4fMJgc1UunqiowO93o&usqp=CAU",
+    phone: "+918 372 574",
+    email: "brad@pitt.com"
+  }
+];
+
+export default contacts;
